e0b753578fc7cc0447053e42e0e83d5c0a631044
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-10-11 00:16:58 +0600

    setup all docker,kubernetes,nginx yaml

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/',( req, res) => {
+    res.send("Hi there!");
+});
+
+app.get('/api/users/currentuser',( req, res) => {
+    res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+});
