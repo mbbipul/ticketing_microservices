8670537f13b6c691e38e7dea4034f803d37549d2
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-10-12 13:24:51 +0600

    Add User Model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+// An interface that describes the properties
+// that are required to create a new user
+
+interface UserAttrs {
+    email : string,
+    password : string
+}
+
+//an interface which describes the properties a user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs : UserAttrs ) : UserDoc;
+}
+
+
+//an interface that describes the propertes that a user document has
+interface UserDoc extends mongoose.Document{
+    email : string,
+    password : string
+}
+
+
+const userSchema = new mongoose.Schema({
+   email : {
+       type : String ,
+       required : true
+   } ,
+   password: {
+       type : String ,
+       required : true
+   }
+});
+
+userSchema.statics.build = ( attrs : UserAttrs) => {
+    return new User(attrs);
+}
+
+const User = mongoose.model<UserDoc,UserModel>('User' , userSchema);
+
+export { User };
