a4804716468d9cb8a92a46100db2bf56931597b4
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-10-12 14:11:10 +0600

    Password hashing and email exists request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body , validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-errors';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -18,16 +20,25 @@ router.post('/api/users/signup', [
         .withMessage('Password must be between 8 and 20 characters')
 ] ,
  async (req : Request , res : Response) => {
+
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
+    
+    const { email,password } = req.body;
 
-    console.log('Creating a user ....');
-    throw new DatabaseConnectionError();
-    res.send({});
+    const isUserExists = await User.findOne({ email });
+
+    if ( isUserExists ){
+        throw new BadRequestError('Email already is used. Pleae try another email');
+    }
+
+    const user = User.build({ email , password});
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
