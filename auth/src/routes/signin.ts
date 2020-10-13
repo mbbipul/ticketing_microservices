366282ad84b430ff26e773d8bc2bf909c59a4dd9
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2020-10-13 13:52:23 +0600

    Complete signin routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express , { Request , Response } from 'express';
 import { body  } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { BadRequestError } from '../errors/bad-request-error';
 import { ValidateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -17,8 +22,36 @@ router.post('/api/users/signin',
             .withMessage('You must supply a password')
     ],
     ValidateRequest,
-    (req : Request, res : Response) => {
+    async (req : Request, res : Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({ email });
+        if (!existingUser){
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        const isPasswordMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
+
+        if (!isPasswordMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        //generate jwt
+        const userJwt = jwt.sign({
+            id : existingUser.id,
+            email : existingUser.email
+        },process.env.JWT_KEY!);
+
+        //store it on session object
+
+        req.session = {
+            jwt : userJwt
+        };
+
+        res.status(200).send(existingUser);
     }
 );
 
